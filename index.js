66dda1f1647935a1450cd0254ca2cc1aabe1de0d
amar-jay <64834413+amar-jay@users.noreply.github.com>
2022-07-15 22:55:24 +0300

    created basic try using html

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("hello")
+const body = document.getElementById('body')
+
+const title = document.createElement('h1')
+title.innerText = "This is the titile"
+title.style.color = '#ccc'
+
+
